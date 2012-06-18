2dfe9e94a2e4e8ed30daa4eb08c6566f06c5112b
Marshall Culpepper <marshall.law@gmail.com>
2012-06-18 12:49:02 -0500

    bump master to 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name="logcat-color",
-      version="0.1",
+      version="0.2",
       description="A colorful alternative to \"adb logcat\"",
       author="Marshall Culpepper",
       author_email="marshall.law@gmail.com",
